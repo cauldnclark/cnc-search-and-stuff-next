2582943f14536e43e2748ba91b87235cb2047c6a
Exx-hub <alvinfloresacosta@gmail.com>
2021-03-21 15:58:43 +0800

    console log test branch

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,41 @@
 import React from "react";
 
 export default function Home() {
-  const [counter, setCounter] = React.useState(0);
-  const [counterTwo, setCounterTwo] = React.useState(0);
-  const [currentCounter, setCurrentCounter] = React.useState(true);
+	const [counter, setCounter] = React.useState(0);
+	const [counterTwo, setCounterTwo] = React.useState(0);
+	const [currentCounter, setCurrentCounter] = React.useState(true);
+	console.log("trial branch upstream");
+	return (
+		<>
+			<h3>JavaScript C&C COUNTER</h3>
+			<h2>{counter}</h2>
 
-  return (
-    <>
-      <h3>JavaScript C&C COUNTER</h3>
-      <h2>{counter}</h2>
+			<h2>{counterTwo}</h2>
 
-      <h2>{counterTwo}</h2>
-
-      <button
-        style={{ marginRight: 20 }}
-        onClick={() =>
-          currentCounter
-            ? setCounter(counter + 1)
-            : setCounterTwo(counterTwo + 1)
-        }
-      >
-        INCREMENT
-      </button>
-      <button
-        onClick={() =>
-          currentCounter
-            ? setCounter(counter - 1)
-            : setCounterTwo(counterTwo - 1)
-        }
-      >
-        DECREMENT
-      </button>
-      <br />
-      <br />
-      <button onClick={() => setCurrentCounter(!currentCounter)}>
-        {`TOGGLE ${currentCounter ? 1 : 2}`}
-      </button>
-    </>
-  );
+			<button
+				style={{ marginRight: 20 }}
+				onClick={() =>
+					currentCounter
+						? setCounter(counter + 1)
+						: setCounterTwo(counterTwo + 1)
+				}
+			>
+				INCREMENT
+			</button>
+			<button
+				onClick={() =>
+					currentCounter
+						? setCounter(counter - 1)
+						: setCounterTwo(counterTwo - 1)
+				}
+			>
+				DECREMENT
+			</button>
+			<br />
+			<br />
+			<button onClick={() => setCurrentCounter(!currentCounter)}>
+				{`TOGGLE ${currentCounter ? 1 : 2}`}
+			</button>
+		</>
+	);
 }
